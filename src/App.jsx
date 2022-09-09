fa19ab1d7549f5ee654dbd6b5e5ab6dc812e4801
Dalton Steele <dsteele92@gmail.com>
2022-09-09 11:08:12 -0700

    Fixed background image so that it does not flash anymore. Added scroll-snap to sections to make a single scroll automatically go to next section. Next going to add third section with work/testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { React, useState, useEffect, useRef } from 'react';
 import Style from './app.module.scss';
 
-import LogoLarge from './assets/logo_large.png';
 import LogoSmall from './assets/logo_small.png';
 import LogoWhite from './assets/THE_FIND_SQUARE_FINAL.png';
-import BannerOne from './assets/banner1.jpg';
+import Banner1 from './assets/banner1.jpg';
+import Banner2 from './assets/banner2.jpg';
+import Banner3 from './assets/banner3.jpg';
 
 import { BsArrowDownCircle, BsLinkedin, BsPhone } from 'react-icons/bs';
 import { AiOutlineMail } from 'react-icons/ai';
@@ -12,21 +13,24 @@ import { AiOutlineMail } from 'react-icons/ai';
 function App() {
 	const [background, setBackground] = useState(1);
 	const header = useRef();
-	// const banner = useRef();
+	const container = useRef();
 
 	useEffect(() => {
+		const containerRef = container.current;
 		const handleScroll = (event) => {
+			console.log('scrolling');
+			console.log(containerRef.scrollTop);
+
+			const currentScroll = containerRef.scrollTop;
+
 			const currentHeight = window.outerWidth > 468 ? 300 : 250;
-			// const windowHeight = window.innerHeight;
-			// console.log(banner.current.style.height);
-			// console.log(window);
-			header.current.style.height = `${currentHeight + window.pageYOffset / 2}px`;
-			header.current.style.opacity = `${1 - window.pageYOffset / 1000}`;
+			header.current.style.height = `${currentHeight + currentScroll / 2}px`;
+			header.current.style.opacity = `${1 - currentScroll / 1000}`;
 		};
-		window.addEventListener('scroll', handleScroll);
+		containerRef.addEventListener('scroll', handleScroll);
 
 		return () => {
-			window.removeEventListener('scroll', handleScroll);
+			containerRef.removeEventListener('scroll', handleScroll);
 		};
 	}, []);
 
@@ -39,17 +43,25 @@ function App() {
 	}, [background]);
 
 	const scrollToBottom = () => {
-		window.scrollTo({
+		console.log(window);
+		console.log(header.current.style);
+		container.current.scrollTo({
 			top: window.outerHeight,
 			left: 0,
 			behavior: 'smooth',
 		});
 	};
 
+	const backgroundImages = [
+		{ id: 1, image: Banner1 },
+		{ id: 2, image: Banner2 },
+		{ id: 3, image: Banner3 },
+	];
+
 	return (
-		<div className={Style.App}>
+		<div className={Style.Container} ref={container}>
 			<section className={Style.Top}>
-				{background === 1 ? (
+				{/* {background === 1 ? (
 					<div className={Style.SlidingBanner1}></div>
 				) : background === 2 ? (
 					<div className={Style.SlidingBanner2}></div>
@@ -57,7 +69,17 @@ function App() {
 					<div className={Style.SlidingBanner3}></div>
 				) : (
 					''
-				)}
+				)} */}
+				{backgroundImages.map((obj) => (
+					<div
+						key={obj.id}
+						className={background === obj.id ? Style[`Background${background}`] : Style.Background}
+						// className={Style[`Background${background}`]}
+						style={{ backgroundImage: `url(${obj.image})` }}></div>
+				))}
+				{/* <div className={Style.Background} style={{ backgroundImage: `url(${Banner1})` }}></div>
+				<div className={Style.Background} style={{ backgroundImage: `url(${Banner2})` }}></div>
+				<div className={Style[`Background${background}`]} style={{ backgroundImage: `url(${Banner3})` }}></div> */}
 				<div className={Style.Overlay}>
 					<img ref={header} className={Style.LogoLarge} src={LogoWhite} alt='Find Firm logo' />
 					<div className={Style.Scroll} onClick={scrollToBottom}>
